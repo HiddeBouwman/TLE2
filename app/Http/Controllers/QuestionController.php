fccbe179e606e11389230f4603b0ce976da9a358
HiddeBouwman <bouwmanhidde@gmail.com>
2025-12-02 15:40:45 +0100

    - Added daily-question.blade.php.
    - Added QuestionController.php, has no real use (yet)
    - Added some default variables to app.css
    - Created variables.css, with styleguide colors and other variables
    - edited nav-link.blade.php focus-visible elements
    - added an aria label to navigation.blade.php
    - added a route to daily-question.blade.php in web.php
    - something happened in package-lock.json, not sure what

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class QuestionController
+{
+    public function show()
+    {
+        $question = [
+            'title' => 'Dummy data 1',
+            'text' => 'Dummy data 2'
+
+        ];
+    }
+}
