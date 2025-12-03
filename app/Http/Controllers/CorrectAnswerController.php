6d48837afc2d3f4e26b6f443ab01fb458fe93a88
Zhi-CMGT <0980617@hr.nl>
2025-12-03 10:13:34 +0100

    added: WrongAnswerController, CorrectAnswerController, views pagina's

---
+++ app/Http/Controllers/CorrectAnswerController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CorrectAnswer;
+use Illuminate\Http\Request;
+
+class CorrectAnswerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CorrectAnswer $correctAnswer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CorrectAnswer $correctAnswer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CorrectAnswer $correctAnswer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CorrectAnswer $correctAnswer)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,23 @@
 <?php
 
+use App\Http\Controllers\CorrectAnswerController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\WrongAnswerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('correctAnswer', [CorrectAnswerController::class, 'index'])->name('correctAnswer');
+
+Route::get('wrongAnswer', [WrongAnswerController::class, 'index'])->name('wrongAnswer');
+
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -17,4 +25,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
