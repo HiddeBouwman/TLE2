d5f7cbf2b389c41b59387d59dab972ffea24f8e2
Thomas van Bockel <Tnvanbockel@gmail.com>
2025-12-03 10:38:20 +0100

    added a image, and made it responsive, fixed that the image is hidden, its not anymore.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,20 +15,28 @@
             </h1>
         </div>
     </div>
-    <div id="fact" class="flex justify-center">
-        <div class="bg-green-900 mt-8  w-4/5  text-4xl">
-            <h1 class=" text-white text-center pt-2">De vliegenzwam</h1>
-            <p class=" text-white  text-center p-8 text-2xl ">
-                De Vliegenzwam is een paddenstoel wat een symbiotische relatie heeft met bomen, de Vliegzwam helpt met
-                meer
-                mineralen naar de boom te transporteren, waar de paddenstoel krijgt daarvoor suikers van de boom.
-            </p>
+    <div id="fact" class="hidden">
+        <div class="flex justify-center">
+            <div class="bg-green-900 mt-8 text-4xl w-4/5  md:h-80 lg:h-60  h-full">
+                <h1 class=" text-white text-center pt-2">De vliegenzwam</h1>
+                <p class=" text-white  text-center p-8 text-2xl ">
+                    De Vliegenzwam is een paddenstoel wat een symbiotische relatie heeft met bomen, de Vliegzwam helpt
+                    met
+                    meer
+                    mineralen naar de boom te transporteren, waar de paddenstoel krijgt daarvoor suikers van de boom.
+                </p>
+            </div>
         </div>
     </div>
-    <div id="fact-image" class="hidden">
-        <img src="" alt="">
-        <p> this is a test image LMAO</p>
-    </div>
+
+    <div id="fact-image" class=" flex justify-center">
+        <div class="bg-green-900 mt-8  lg:h-60 w-4/5 md:h-80 text-4xl h-full object-cover">
+
+            <img class="object-cover opacity-70 w-full h-full"
+                 src="{{ Vite::asset('resources/images/vliegenzwam.webp') }}"
+                 alt="Foto van de vliegenzwam">
+
+        </div>
 
 
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/landingPage', function () {
+Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
