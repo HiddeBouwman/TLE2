fd27392d16e3f01cd30e06fcb56587c1e8d34501
Thomas van Bockel <Tnvanbockel@gmail.com>
2025-12-03 11:48:03 +0100

    added variable in css, added sections to my code for semantic, added button to dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,16 +8,16 @@
          voor screenreader de fact automatisch laten zien.
          --}}
 
-        <div id="info" class="flex justify-center">
-            <div class="bg-pink-600 w-1/2 mt-10 rounded ">
+        <section id="info" class="flex justify-center">
+            <div class=" bg-primary w-1/2 mt-10 rounded ">
                 <h1 class=" text-white  dark: text-center pb-4 pt-4  text-4xl">
                     {{ __('klik voor de feit van de dag!') }}
                 </h1>
             </div>
-        </div>
-        <div id="fact" class="hidden">
+        </section>
+        <section id="fact" class="hidden">
             <div class="flex justify-center">
-                <div class="bg-green-900 mt-8 text-4xl w-4/5  md:h-80 lg:h-60  h-full">
+                <div class="bg-accent mt-8 text-4xl w-4/5  md:h-80 lg:h-60  h-full">
                     <h2 class=" text-white text-center pt-2">De vliegenzwam</h2>
                     <p class=" text-white  text-center p-8 text-2xl ">
                         De Vliegenzwam is een paddenstoel wat een symbiotische relatie heeft met bomen, de Vliegzwam
@@ -29,16 +29,23 @@
                     </p>
                 </div>
             </div>
-        </div>
+        </section>
 
-        <div id="fact-image" class=" flex justify-center">
-            <div class="bg-green-900 mt-8  lg:h-60 w-4/5 md:h-80 text-4xl h-full object-cover">
+        <section id="fact-image" class=" flex justify-center">
+            <div class="bg-accent mt-8  lg:h-60 w-4/5 md:h-80  h-full object-cover">
 
-                <img class="object-cover opacity-70 w-full h-full"
+                <img class="object-cover opacity-85 w-full h-full"
                      src="{{ Vite::asset('resources/images/vliegenzwam.webp') }}"
                      alt="Foto van de vliegenzwam">
 
             </div>
-        </div>
+        </section>
+
+        <a href="{{ route('daily-question') }}"
+           class=" bg-primary text-white text-xl py-3 px-12 ml-40 mt-20 inline-block hover:primary-hover transition duration-300">
+
+            Naar vraag
+
+        </a>
     </x-slot>
 </x-app-layout>
