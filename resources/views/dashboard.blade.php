535dc8b4eb4ce59a9d63167e70f4ee590c4ed64b
Thomas van Bockel <Tnvanbockel@gmail.com>
2025-12-08 10:16:45 +0100

    fixed the text  so it is better to understand for the users.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -46,18 +46,19 @@
             <section id="text">
                 <div class="flex justify-center lg:justify-start">
                     <div class="lg:w-30 bg-gradient-lap mx-4 text-2xl lg:text-xl lg:ml-40 mt-10 shadow-lg p-5">
-                        <ul class="list-disc list-inside text-white">
+                        <ul class="list-disc list-inside text-white ">
                             <li>
-                                Lees het weetje!
+                                Lees de feit van de dag!
                             </li>
                             <li>
                                 Doe de opdracht!
                             </li>
+
                             <li>
-                                Behoud de streak!
+                                Behaal prijzen door de reeks te behouden!
                             </li>
                             <li>
-                                Behaal prijzen!
+                                Behoud de reeks voor 30 dagen en verdien een boom met jouw naam!
                             </li>
                         </ul>
                     </div>
